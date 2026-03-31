fc02584a8344e78400eac4071921d9af0fa057ce
almenglee <aclyung1234556@gmail.com>
2026-03-31 15:29:53 +0900

    add cmd/debug content

--- cmd/debug/main.go
+++ cmd/debug/main.go
@@ -1,6 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"log"
+)
+
 func main() {
-	// TODO: get path from args
+	path := "draft.md"
+	builder, err := NewDocumentBuilder(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := builder.Build(); err != nil {
+		log.Fatal(err)
+	}
+
+	for _, line := range builder.lines {
+		fmt.Println(line)
+	}
 
 }
